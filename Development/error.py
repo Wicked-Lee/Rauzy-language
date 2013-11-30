07b34b748f84eac23d65a3bf09f86d89f954deb0
chaogang.li <wicked@wicked-Ideapad-V460.(none)>
2013-11-30 03:23:20 +0100

    I have programed a part of val_root, function to check if the root object is well formatted or not. Two classes were added:error and warning

---
+++ Development/error.py
@@ -0,0 +1,9 @@
+#define a Error class to raise user-defined errors.
+class error(Exception):
+    #"msg" is the error message that we should print
+    def __init__(self,msg):
+        self.msg=msg
+    #the fonction to print the error message
+    def toStr(self):
+        print(self.msg)
+        
