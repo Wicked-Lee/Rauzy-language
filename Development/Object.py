ca378319d7408d3e9a66d8329bf41fb01eab86e4
chaogang.li <wicked@wicked-Ideapad-V460.(none)>
2013-12-04 00:16:18 +0100

    The val_lib function is finished except the handling of cyclic dependency error and the debug...

--- Development/Object.py
+++ Development/Object.py
@@ -15,15 +15,25 @@ class Object:
 	#TODO handle "Error 03: the object xx is not defined !"
 	@staticmethod	
 	def findObject(name):
+            try:
 		if name in Object.objects.keys() :
 			return Object.objects[name]
+		else:
+                        raise error("Error 03: the object "+name+" is not defined !")
+            except error as e:
+                e.toStr()
 		
 	#finds the relation with the given name	
         #TODO handle "Error 04:the relation xx is not defined !"
 	@staticmethod
 	def findRelation(name):
+            try:
 		if name in Object.relations.keys() :
 			return Object.relations[name]
+		else:
+                        raise error("Error 04:the relation "+name+" is not defined !")
+            except error as e:
+                    e.toStr()
 	
 	@staticmethod
 	def addObject(name,object):
@@ -31,14 +41,20 @@ class Object:
 	@staticmethod
 	def addRelation(name,value):
 		Object.relations[name]=value
-	
+
+	#handle "Error 02:the library file xx is not found!"
 	@staticmethod
 	def readLibrary(file):
 		f=open(file,'r')        #open a file for reading
 		s=f.read()              #read the content of file f
+		#To get rid of all the unmeaningful symbols after reading
+                s=s.replace("\n","")    #replace the string "\n" by ""
+                s=s.replace("\t","")    #replace the string "\t" by ""
+                s=" ".join(s.split())   #remove the duplicated spaces
 		#return a python object out of a json object
 		target=json.loads(s)
 		f.close()
+		#val_lib(file,target)
 		for key in target.keys():			
 			if key == "objects":
 				for objectName in target[key].keys():
@@ -53,18 +69,70 @@ class Object:
     
 
 #TODO handle "Error 01: xx in the xx not defined!"
-#handle "Error 02:the library file xx is not found!"
 #handle "Error 05:the nature of xx is not correct!"
 #handle "Error 06:A cyclic dependency detected! The cycle is xx0-(include)->xx1-(extends)->xx2-(include)->..-(extends)->xx0."
 #handle "Error 07:Redundant definition of xx!"
-#handle "Error 08:Incorrect library file format:(absence of xx)|(xx not recognized)!" 
+#handle "Error 08:Incorrect library file format:(xx not recognized)!" 
 #handle "Warning 01:xx should not be defined in a libray relation."
 #handle "Warning 03: 'objects' and 'relations' in object A will be overriden by these of object B as A extends B"
 	@staticmethod
-	def val_lib(target):
-		pass 
-				
-				
+	def val_lib(tarname,target,islib,list_obj,list_rel):
+                err_str=""
+                war_str=""
+                if 'nature' not in target.keys() or target['nature']=='':
+                        err_str+='Error01: the field [nature] is not defined in the library '+tarname+'!\n'
+                elif target["nature"] != "library" and islib:
+                        err_str+='Error 05:the nature of library '+tarname+' is not correct!\n'
+                elif target['nature'] !='object' and not islib:
+                        err_str+='Error 05:the nature of object '+tarname+' is not correct!\n'
+                if 'objects' in target.keys() and target['objects'] != '':
+                        for key in target['objects'].keys():
+                                #with below, we can check "Error 07" at the different levels
+                                if key in list_obj:
+                                        err_str+='Error 07:Redundant definition of object '+key+'!\n'
+                                else:
+                                        list_obj.append(key)
+                                valstr=val_lib(key,target['objects'][key],False,list_obj,list_rel)
+                                err_str+=valstr[0]
+                                war_str+=valstr[1]
+                if islib and 'relations' in target and target['relations']!='':
+                        for key in target['relations'].keys():
+                                if 'nature' not in target['relations'][key]:
+                                        err_str+='Error01: the field [nature] is not defined in'+tarname+'[relations]['+key+']\n'
+                                elif target['relations'][key]['nature'] != 'relation':
+                                        err_str+='Error 05:the nature of'+tarname+'[relations]['+key+'] is not correct!\n'
+                                if 'from' in target['relations'][key]:
+                                        war_str+='Warning 01: the field [from] should not be defined in '+tarname+'[relations]['+key+']\n'
+                                if 'to' not in target['relations'][key]:
+                                        war_str+='Warning 01: the field [to] should not be defined in '+tarname+'[relations]['+key+']\n'
+                                if 'directional' not in target['relations'][key]:
+                                        err_str+='Error 01: the field [directional] is not defined in '+tarname+'[relations]['+key+']\n'
+                if not islib and 'relations' in target and target['relations']!='':
+                        for key in target['relations'].keys():
+                                #with below, we can check "Error 07" at the different levels
+                                if key in list_rel:
+                                        err_str+='Error 07:Redundant definition of relation '+key+'!\n'
+                                else:
+                                        list_rel.append(key)
+                                if 'nature' not in target['relations'][key]:
+				        err_str+='Error 01: the field [nature] is not defined in lib['+tarname+'][relations]['+key+']\n'
+                                elif target['relations'][key]['nature'] != 'relation':
+                                        err_str+='Error 05:the nature of'+tarname+'[relations]['+key+'] is not correct!\n'
+                                if 'from' not in target['relations'][key]:
+                                        err_str+='Error 01: the field [from] is not defined in '+tarname+'[relations]['+key+']\n'
+                                if 'to' not in target['relations'][key]:
+                                        err_str+='Error 01: the field [to] is not defined in '+tarname+'[relations]['+key+']\n'
+                                if ('extends' not in target['relations'][key] or target['relations'][key]['extends']=='') and 'directional' not in target['relations'][key]:
+                                        err_str+='Error 01: the field [directional] is not defined in '+tarname+'[relations]['+key+']\n'
+                if islib:
+                        for key in target.keys():
+                            try:
+                                if key not in ['nature','objects','relations']:
+                                        raise error("Error 08:Incorrect library file format: "+key+" not recognized!\n")
+                            except error as e:
+                                e.toStr()
+                                #err_str+=e.msg
+                        				
 	#properties dictionary
 	#objects list of strings
 	#relations list of strings	
