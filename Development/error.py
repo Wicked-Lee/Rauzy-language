f93d454e4c8042b7ca87ca4a04df712ad0b2841e
Georgios Papoutsakis <g.papoutsakis86@gmail.com>
2014-04-25 11:36:16 +0200

    Removes error.py warning.py.

--- Development/error.py
+++
@@ -1,9 +0,0 @@
-#define a Error class to raise user-defined errors.
-class error(Exception):
-    #"msg" is the error message that we should print
-    def __init__(self,msg):
-        self.msg=msg
-    #the fonction to print the error message
-    def toStr(self):
-        print(self.msg)
-
--- Development/warning.py
+++
@@ -1,9 +0,0 @@
-#define a warning class to raise user-defined Warnings.
-class warning(Exception):
-    #"msg" is the warning message that we should print
-    def __init__(self,msg):
-        self.msg=msg
-    #the fonction to print the warning message
-    def toStr(self):
-        print(self.msg)
-
